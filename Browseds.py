2c6a671baf925f35590f8c844fe635090bba41ab
akopylov <kopylov.anton@gmail.com>
2020-03-24 00:23:32 +0300

    first

---
+++ Browseds.py
@@ -0,0 +1,88 @@
+import pafy
+#from tqdm import tqdm
+# import tensorflow as tf
+# print('tensorflow.__version__', tf.__version__)
+import cv2
+print('cv2 version',cv2.__version__)
+import tkinter as tk
+import tkinter.filedialog
+from  PIL import Image, ImageTk
+# import datetime
+# import os
+import pickle
+
+
+class Application:
+    def __init__(self, output_path = "./"):
+        self.root = tk.Tk()  # initialize root window
+        self.root.bind("<Key>", self.keyevent)
+        self.root.title("PyImageSearch")  # set window title
+        # self.destructor function gets fired when the window is closed
+        self.root.protocol('WM_DELETE_WINDOW', self.destructor)
+
+        self.tframeB = tk.Frame(self.root)
+        self.tframeB.pack(side=tk.TOP,fill=tk.BOTH)
+
+        self.tframeV = tk.Canvas(self.root)
+        self.tframeV.pack(side=tk.LEFT, fill=tk.BOTH)
+
+        self.panel = tk.Label(self.tframeV)  # initialize image panel
+        #self.panel.bind("<Button-1>", self.Button1)
+        self.panel.pack(padx=10, pady=10)
+
+        self.btn = tk.Button(self.tframeB, text="(space)Next!", command=self.next, height = 5,width = 10)
+        self.btn.pack(side=tk.LEFT, expand=False, padx=5, pady=5)
+        # self.btnp = tk.Button(self.tframeB, text="Load!", command=self.load, height = 5,width = 10)
+        # self.btnp.pack(side=tk.LEFT, expand=False, padx=5, pady=5)
+        self.cnt=0
+        self.filename = tk.filedialog.askopenfilename(initialdir = "./",title = "Select file",filetypes = (("jpeg files","*.pkl"),("all files","*.*")))
+        print('file ', self.filename)
+        self.load()
+    def keyevent(self, event):
+        if event.char == ' ':
+            self.next()
+    def load(self):
+        with open(self.filename, 'rb') as f:
+            self.data = pickle.load(f)
+    def next(self):
+
+        img = self.data[self.cnt][1]
+        if self.data[self.cnt][0]==1:
+            clr=(0,255,255)
+        else:
+            clr = (0, 0, 255)
+        img = cv2.resize(img, (400, 400))
+        cv2.rectangle(img, (5, 5), (400-5, 400-5), clr, 2)
+        self.schow_frame(img,self.panel)
+
+        self.cnt += 1
+        if self.cnt == len(self.data):
+            self.cnt = 0
+
+
+
+    def schow_frame(self,cv2image,panel):
+        # cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)  # convert colors from BGR to RGBA
+
+        self.current_image = Image.fromarray(cv2image)  # convert image for PIL
+        imgtk = ImageTk.PhotoImage(image=self.current_image)  # convert image for tkinter
+        panel.imgtk = imgtk  # anchor imgtk so it does not be deleted by garbage-collector
+        panel.config(image=imgtk)  # show the image
+
+
+    def destructor(self):
+        """ Destroy the root object and release all resources """
+        print("[INFO] closing...")
+        self.root.destroy()
+        cv2.destroyAllWindows()  # it is not mandatory in this application
+
+# # construct the argument parse and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-o", "--output", default="./",
+#     help="path to output directory to store snapshots (default: current folder")
+# args = vars(ap.parse_args())
+
+# start the app
+print("[INFO] starting...")
+pba = Application()
+pba.root.mainloop()
---
+++ newds.py
@@ -0,0 +1,256 @@
+import pafy
+#from tqdm import tqdm
+# import tensorflow as tf
+# print('tensorflow.__version__', tf.__version__)
+import cv2
+print('cv2 version',cv2.__version__)
+import tkinter as tk
+
+from  PIL import Image, ImageTk
+import datetime
+import os
+import pickle
+
+
+class Application:
+    def __init__(self):
+
+        self.root = tk.Tk()  # initialize root window
+        self.root.bind("<Key>", self.keyevent)
+        self.root.title("PyImageSearch")  # set window title
+        # self.destructor function gets fired when the window is closed
+        self.root.protocol('WM_DELETE_WINDOW', self.destructor)
+
+        self.tframeB = tk.Frame(self.root)
+        self.tframeB.pack(side=tk.TOP,fill=tk.BOTH)
+
+        self.tframeV = tk.Canvas(self.root)
+        self.tframeV.pack(side=tk.LEFT, fill=tk.BOTH)
+
+        self.panel = tk.Label(self.tframeV)  # initialize image panel
+        self.panel.bind("<Button-1>", self.Button1)
+        self.panel.pack(padx=10, pady=10)
+
+        self.tframeF = tk.Frame(self.root)
+        self.tframeF.pack(side=tk.LEFT,fill=tk.BOTH)
+        self.panelF = tk.Label(self.tframeF)  # initialize image panel
+        self.panelF.pack(padx=10, pady=10)
+
+        # create a button, that when pressed, will take the current frame and save it to file
+        # create a button, that when pressed, will take the current frame and save it to file
+        self.btnt = tk.Button(self.tframeB, text="TrackIt!", command=self.trackit, height = 5,width = 10)
+        self.btnt.pack(side=tk.RIGHT, expand=False, padx=5, pady=5)
+        self.btnt = tk.Button(self.tframeB, text="Save!", command=self.take_snapshot, height = 5,width = 10)
+        self.btnt.pack(side=tk.RIGHT, expand=False, padx=5, pady=5)
+        # create a button, that when pressed, will take the current frame and save it to file
+        self.btnt = tk.Button(self.tframeB, text="PICK!", command=self.pick, height = 5,width = 10)
+        self.btnt.pack(side=tk.LEFT, expand=False, padx=5, pady=5)
+        self.btn = tk.Button(self.tframeB, text="Next!", command=self.countur_loop, height = 5,width = 10)
+        self.btn.pack(side=tk.LEFT, expand=False, padx=5, pady=5)
+        self.btnp = tk.Button(self.tframeB, text="Start!", command=self.pause, height = 5,width = 10)
+        self.btnp.pack(side=tk.LEFT, expand=False, padx=5, pady=5)
+        self.yadress = tk.Entry(self.tframeB, text="Youtube IDT")
+        self.yadress.insert(0,'fdqOdTvGc9I')
+        self.url = None
+        self.yadress.pack(side=tk.LEFT, expand=False, padx=5, pady=5)
+        self.isPause = True
+        # Tracker
+        self.buf = {}  # current frame from stream
+        self.buf['initBB']=None
+        self.buf['frame1']=None
+        self.buf['countur_n'] = -1
+        self.picked = []
+        self.nextpick= []
+        self.backSub = cv2.createBackgroundSubtractorKNN()
+
+        # start a self.video_loop that constantly pools the video sensor
+        # for the most recently read frame
+        self.video_loop()
+
+    def startVS(self):
+        if self.buf['frame1'] is not None:
+            self.vs.release()  # release web camera
+            cv2.destroyAllWindows()
+
+        """ Initialize application which uses OpenCV + Tkinter. It displays
+            a video stream in a Tkinter window and stores current snapshot on disk """
+        # Get youtube stream url by IDT
+        # WHWlRaRAtbU - баскет
+        # fdqOdTvGc9I - волейбол
+        #self.url = 'fdqOdTvGc9I'
+        vPafy = pafy.new(self.url)
+        play = vPafy.getbest()
+        #print(play.resolution, play.extension, play.get_filesize())
+
+        # initialize the video stream
+        self.vs = cv2.VideoCapture(play.url) # capture video frames, 0 is your default video camera
+        self.output_path = './'  # store output path
+        self.buf['frame1']=None
+        self.buf['countur_n'] = -1
+        self.frame_width = int(self.vs.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.frame_height = int(self.vs.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.fps = int(self.vs.get(cv2.CAP_PROP_FPS))
+    def keyevent(self, event):
+        if event.char == ' ':
+            self.pause()
+        if event.char == 's':
+            self.pick()
+        if event.char == 'd':
+            self.take_snapshot()
+    def Button1(self,event):
+        if self.isPause:
+            #print("clicked at", event.x, event.y)
+            for (i,(x, y, w, h)) in enumerate(self.buf['contoursFilered']):
+                #(x, y, w, h)=self.buf['contoursFilered'][i]
+                if 2*event.x>x-5 and 2*event.x<x+w+5:
+                    if 2*event.y>y-5 and 2*event.y < y +h +5:
+                        img = self.buf['frame1'].copy()
+                        self.buf['countur_n']=i
+                        (img1, crop_img) = self.__drawrect(img, self.buf['contoursFilered'][i],
+                                                          (0, 255, 0))
+                        tf=crop_img.copy()
+                        self.schow_frame(tf, self.panelF)
+
+                        (img2, crop_img) = self.__drawrect(img1, self.buf['contoursFilered'][self.__nextframe()],
+                                                          (0, 0, 255))
+                        ff = crop_img.copy()
+                        self.nextpick=[ff,tf]
+                        self.schow_frame(img2, self.panel)
+                        break
+            #self.pause()
+    def pause(self):
+        if self.isPause:
+            self.btnp["text"] = "Pause!"
+            self.isPause = False
+            self.yadress.config(state = 'readonly')
+            if self.url != str(self.yadress.get()):
+                self.url = str(self.yadress.get())
+                self.startVS()
+        else:
+            #self.yadress.config(state=tk.NORMAL)
+            self.btnp["text"] = "Resume!"
+            self.isPause = True
+    def __nextframe(self):
+        i = self.buf['countur_n'] + 1
+        if i == len(self.buf['contoursFilered']):
+            i = 0
+        return i
+    def pick(self):
+        if  self.isPause and len(self.nextpick)==2:
+            self.picked.append([1,self.nextpick[1]])
+            self.picked.append([0,self.nextpick[0]])
+            self.nextpick=[]
+            print('Picked {}'.format(len(self.picked)))
+            if len(self.picked)%10==0:
+                self.take_snapshot()
+            self.pause()
+    def countur(self):
+        if type(self.buf['frame1']) == type(None):
+            ret, self.buf['frame1'] = self.vs.read()
+        else:
+            self.buf['frame1'] = self.buf['frame2'].copy()
+
+        ret, self.buf['frame2'] = self.vs.read()
+
+        diff = cv2.absdiff(self.buf['frame1'], self.buf['frame2'])
+        diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
+        diff = cv2.blur(diff, (10, 10))
+        _, diff = cv2.threshold(diff, 20, 255, cv2.THRESH_BINARY)
+
+
+        # grey1 = cv2.cvtColor(self.buf['frame1'], cv2.COLOR_BGR2GRAY)
+        # diff = self.backSub.apply(grey1)
+        # diff=cv2.blur(diff,(10,10))
+        # _, diff = cv2.threshold(diff, 20, 255, cv2.THRESH_BINARY)
+
+        # kernel = cv2.getStructuringElement(shape=cv2.MORPH_ELLIPSE, ksize=(5, 5))
+        # diff = cv2.morphologyEx(diff, cv2.MORPH_CLOSE, kernel, iterations=7)
+        diff = cv2.dilate(diff, None, iterations=3)
+
+        contours, _ = cv2.findContours(diff, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        self.buf['contoursFilered'] = []
+        self.buf['countur_n']=-1
+        self.buf['frame_contur'] = self.buf['frame1'].copy()
+        if self.buf['initBB'] is not None:
+            self.tracknext(self.buf['frame_contur'])
+        for contour in contours:
+            self.buf['countur_n'] = 0
+            (x, y, w, h) = cv2.boundingRect(contour)
+            s = cv2.contourArea(contour)
+            if s < 50 or s > 1000:
+                continue
+            self.buf['contoursFilered'].append((x, y, w, h))
+            cv2.rectangle(self.buf['frame_contur'], (x , y), (x + w, y + h), (0, 255, 255), 2)
+            # cv2.putText(self.buf['frame_contur'], "s={}".format(str(s)), (x + w + 5, y), cv2.FONT_HERSHEY_SIMPLEX,
+            #             0.5, (0, 255, 255), 3)
+    def tracknext(self,frame):
+        # check to see if we are currently tracking an object
+        if self.buf['initBB'] is not None:
+            # grab the new bounding box coordinates of the object
+            (success, box) = self.tracker.update(frame)
+            # check to see if the tracking was a success
+            #print('track',success)
+            if success:
+                (x, y, w, h) = [int(v) for v in box]
+                self.buf['initBB'] = (x, y, w, h)
+                cv2.rectangle(frame, (x, y), (x + w, y + h),
+                              (0, 255, 0), 2)
+    def trackit(self):
+        if  self.isPause and self.buf['countur_n'] >= 0:
+            self.buf['initBB']=self.buf['contoursFilered'][self.buf['countur_n']]
+            # start OpenCV object tracker using the supplied bounding box
+            # coordinates, then start the FPS throughput estimator as well
+            self.tracker = cv2.cv2.TrackerKCF_create()
+            # cv2.TrackerMOSSE_create
+            # cv2.TrackerCSRT_create
+            # TrackerKCF_create()
+            self.tracker.init(self.buf['frame1'], self.buf['initBB'])
+    def schow_frame(self,frame,panel):
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)  # convert colors from BGR to RGBA
+        cv2image = cv2.resize(cv2image,(int(self.frame_width//2),int(self.frame_height//2)))
+        self.current_image = Image.fromarray(cv2image)  # convert image for PIL
+        imgtk = ImageTk.PhotoImage(image=self.current_image)  # convert image for tkinter
+        panel.imgtk = imgtk  # anchor imgtk so it does not be deleted by garbage-collector
+        panel.config(image=imgtk)  # show the image
+
+    def video_loop(self):
+        if not self.isPause:
+            self.countur()
+            self.schow_frame(self.buf['frame_contur'],self.panel)
+        self.root.after(10, self.video_loop)  # call the same function after 30 milliseconds
+
+    def __drawrect(self,img,cont,color):
+        (x, y, w, h) = cont
+        crop_img = img[y:y + h, x:x + w].copy()
+        #crop_img = cv2.resize(crop_img, ((w) * 2, (h) * 2))
+        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+        return img.copy(),crop_img.copy()
+    def countur_loop(self):
+        #print(self.isPause , self.buf['countur_n'])
+        if self.isPause and len(self.buf['contoursFilered']) >= 0:
+            self.buf['countur_n'] = self.__nextframe()
+            img=self.buf['frame1'].copy()
+            (img, crop_img) =self.__drawrect(img,self.buf['contoursFilered'][self.buf['countur_n']],(0, 255, 255))
+            self.schow_frame(crop_img, self.panelF)
+            self.schow_frame(img, self.panel)
+
+    def take_snapshot(self):
+        """ Take snapshot and save it to the file """
+        ts = datetime.datetime.now() # grab the current timestamp
+        filename = "{}.pkl".format(ts.strftime("%Y-%m-%d_"+self.url))  # construct filename
+        with open(filename, 'wb') as f:
+            pickle.dump(self.picked,f)
+        print('data saved')
+
+    def destructor(self):
+        """ Destroy the root object and release all resources """
+        print("[INFO] closing...")
+        self.root.destroy()
+        self.vs.release()  # release web camera
+        cv2.destroyAllWindows()  # it is not mandatory in this application
+
+
+# start the app
+print("[INFO] starting...")
+pba = Application()
+pba.root.mainloop()
